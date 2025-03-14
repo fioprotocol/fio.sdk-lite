754a7bba60731dbd0af17ba3a0e268bb69b8f726
Oleksii Trukhanov <trukhantech@gmail.com>
2025-03-14 16:40:12 +0200

    DASH-1344 Separate tests into own files.

--- jest.config.ts
+++ jest.config.ts
@@ -152,15 +152,10 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: ['<rootDir>/src/tests/index.test.ts'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ['/node_modules/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
